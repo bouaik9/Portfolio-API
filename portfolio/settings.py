b601b7def6b499490532143fed49d25737a159bb
bouaik9 <113983675+bouaik9@users.noreply.github.com>
2024-01-11 13:58:16 +0100

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -12,8 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
-import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -134,4 +133,3 @@ REST_FRAMEWORK = {
     ],
 }
 
-django_heroku.settings(locals())
